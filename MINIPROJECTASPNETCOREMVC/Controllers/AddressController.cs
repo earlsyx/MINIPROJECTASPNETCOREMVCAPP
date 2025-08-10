61889873b1ef765193ada22b18828293cd59d639
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-08-10 22:44:28 +0800

    Create address and person controller only with get and create and create post for both. - add view for both

---
+++ MINIPROJECTASPNETCOREMVC/Controllers/AddressController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MINIPROJECTASPNETCOREMVC.Controllers
+{
+    public class AddressController : Controller
+    {
+        // GET: Address
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Address/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Address/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MINIPROJECTASPNETCOREMVC/Controllers/PersonController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MINIPROJECTASPNETCOREMVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof( Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    
+    }
+}
